27a3824d34bcfbb1d3f1f43eb9956115a1e35619
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:14:10 +0200

    serra

---
+++ stato_serra/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace stato_serra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci la temperatura della serra:  ");
+            double temperatura = double.Parse(Console.ReadLine());
+            if (temperatura < 5)
+                Console.Write("La serra ha dei danni irreparabili");
+            else if (temperatura > 5 && temperatura < 18)
+                Console.Write("La serra è in una situazione di pericolo");
+            else
+                Console.Write("La serra ha una temperatura normale");
+            Console.ReadLine();
+        }
+    }
+}
